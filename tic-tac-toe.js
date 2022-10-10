ac59e137bfb7b79f2fd9c412511d3b146264aa5e
Tiffany Parkinson <banana13.tp@gmail.com>
2022-10-09 20:37:07 -0500

    Updated tic-tac-toe.js\

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,9 +3,34 @@ onload = function(){
     let board = document.getElementById("board");
     let boxes = board.querySelectorAll("div");
 
+    //Array to keep track of where each player is
+    let gameState = ["", "", "", "", "", "", "", "", ""];
+
+    //Counter to keep track of each play to see if it is even(X) or odd(O)
+    let counter = 0;
+
     //Excercise 1
     boxes.forEach(function(elem, index, list) {
         elem.setAttribute("class", "square");
-        console.log("jah");
+
+        //Excercise 2
+        elem.addEventListener("click", function(){
+
+            if(counter%2===0 && elem.innerHTML===""){
+                elem.innerHTML="X";
+                elem.classList.add("square", "X");
+
+                counter++;
+                gameState[index]="X";
+            }
+
+            else if(counter%2===1 && elem.innerHTML===""){
+                elem.innerHTML="O";
+                elem.classList.add("square", "O");
+
+                counter++;
+                gameState[index]="O";
+            }
+        });    
     });
 }
